e26be88b5cb535b2b73af0dc5bf6ba66b3cc54b2
mattgaither <benjamin.gaither@shipt.com>
2022-04-09 21:18:55 -0500

    learning some javascript commands/elements

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,24 @@
-window.alert("This is an alert! JavaScript is running!");
+var playername = window.prompt("What is your robot's name?")
+
+console.log(playername);
+
+console.log("This logs a string, good for leaving yourself a messsage");
+
+//this will do math and log 20
+
+console.log(10+10);
+
+//what is this?
+
+console.log("Our robot's name is " + playername);
+
+// this creates the function name "fight"
+
+function fight(){
+    window.alert("The fight has begun!")
+}
+
+//fight()
+
+var name = "your name";
+console.log(name)
