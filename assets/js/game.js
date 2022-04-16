ca68797381af67b9d234aebe9e172a8b0882bd54
mattgaither <benjamin.gaither@shipt.com>
2022-04-16 02:23:03 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -32,14 +32,16 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney)
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - playerAttack);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -57,7 +59,10 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+
+    playerHealth = Math.max(0, playerHealth - enemyAttack);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -86,6 +91,10 @@ var startGame = function(){
 
       // pick new enemy to fight based on the enemyNames array 
       var pickedEnemyName = enemyNames[i];
+
+      enemyHealth = randomNumber(40,60);
+
+      fight(pickedEnemyName);
       
       fight(pickedEnemyName);
       if(playerHealth > 0 && i < enemyNames.length -1) {
@@ -153,6 +162,14 @@ var shop = function(){
 
   }
 };
+
+// fuction to generate a random numeric value
+var randomNumber = function(min,max) {
+  var value =  Math.floor(Math.random() * (21)) + 40;
+
+  return value;
+}
+
 
 //start the game when the page loads 
 startGame();
